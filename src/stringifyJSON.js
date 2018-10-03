c028a16c663af731a32e3cb465e161b2a69865ae
Felipe <phil.sousa@gmail.com>
2018-10-02 19:26:25 -0700

    complete bare minimum requirements

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,26 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (Array.isArray(obj)) {
+    var results = [];
+    for (let i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+    }
+    return '[' + results.join(',') + ']';
+  }
+
+  if (obj && typeof obj === 'object') {
+    var results = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function') {
+        return undefined
+      }
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + results.join(',') + '}';
+  }
+  if(typeof obj === "string") {
+    return '"' + obj + '"';
+  }
+  return "" + obj; 
 };
